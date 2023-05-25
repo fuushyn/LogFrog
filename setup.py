42d7efbaf5d2b633d91a20ed4bf46a180f70f108
Lakshya Gupta <gzlakshya@gmail.com>
2023-05-26 02:26:00 +0530

    added package to pip

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
 from setuptools import setup, find_packages
 
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="logfrog",
-    version="0.1",
-    packages=find_packages(),
-    description="A simple and efficient logging library.",
+    version="1.0.0",
     author="Lakshya Gupta",
-    author_email="lakshyagupta831@gmail.com",
+    description="A simple and efficient logging package in python with async support",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/fuushyn/logfrog",
+    packages=find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
 )
