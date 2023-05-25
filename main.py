732975d28837bd6a7c860851f2e026c0fc7b2468
Lakshya Gupta <gzlakshya@gmail.com>
2023-05-26 00:58:51 +0530

    got map reduce to work

--- main.py
+++ main.py
@@ -1,46 +1,35 @@
 import datetime
-from threading import Lock
 from threading import Thread 
 from queue import Queue 
-import time 
 
 class PyLog:
-    def __init__(self, filename= "logs.txt"):
+    def __init__(self, filename= "logs.txt"):   
         self.filename = filename
-        self.lock = Lock()
         self.log_queue = Queue()
-        self.thread = Thread(target=self._background_logger)
+        self.thread = Thread(target=self._background_logger, daemon = True)
         self.thread.start()
 
     #background logger for async logging
     def _background_logger(self):
         while(True):
-            time.sleep(1)
-
-            item = self.log_queue.get()
-            print(item)
+            item = self.log_queue.get(block=True)
             if(item is None):
+                self.index = -1 
                 break
             
-            print(item)
-
-            if(self.log_queue.empty()):
-                continue
             log_level, message, timestamp = item
-            print(item)
             with open(self.filename, 'a+') as log_file:
                 print(f"{timestamp} '{log_level}' '{message}'\n")
                 log_file.write(f"{timestamp} '{log_level}' '{message}'\n")
 
+
     def stop(self):
         self.log_queue.put(None)
-        self.thread.join()
         
     def write_log(self, message, log_level ="INFO"):
         timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         print("pushing to queue")
-        self.log_queue.put((log_level, message, timestamp), block=False)
-        # self.log_queue.join()
+        self.log_queue.put((log_level, message, timestamp))
 
     def info(self, message):
         self.write_log("INFO", message)
